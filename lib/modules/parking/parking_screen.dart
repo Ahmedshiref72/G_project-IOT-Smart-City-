1349fd2d26d3072197f0359bdf8d843eaf22deb2
Ahmed Shiref <103084673+Ahmedshiref72@users.noreply.github.com>
2022-04-06 03:50:43 +0200

    Ahmedshiref

--- lib/modules/parking/parking_screen.dart
+++ lib/modules/parking/parking_screen.dart
@@ -170,7 +170,7 @@ Widget buildChatItem(
                                 child: Text(
                                   '${city}',
                                   style: TextStyle(
-                                      fontSize: 16.0,
+                                      fontSize: 17.0,
                                       fontWeight: FontWeight.bold),
                                   maxLines: 1,
                                   overflow: TextOverflow.ellipsis,
