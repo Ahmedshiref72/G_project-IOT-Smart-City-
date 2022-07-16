497c5c934c1129adda6280707194088962117f54
Ahmed Shiref <103084673+Ahmedshiref72@users.noreply.github.com>
2022-07-16 14:39:09 +0200

    pass model

--- lib/modules/About_us/about_us_screen.dart
+++ lib/modules/About_us/about_us_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smart_city/modules/password/gatePassword.dart';
+import 'package:smart_city/shared/components/components.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutUsScreen extends StatelessWidget {
@@ -7,8 +9,119 @@ class AboutUsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('About Us '),),
       body: Center(
-        child: TextButton(
-          onPressed: ()async
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text('Connect Us',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+            SizedBox(
+              height: 50,
+            ),
+            TextButton(onPressed: ()async
+            {
+              const url = "https://flutter.io";
+              if (await canLaunch(url) != null)
+                await launch(url);
+              else
+                // can't launch url, there is some error
+                throw "Could not launch $url";
+
+            },
+                child: Text('Ahmed Ehab Abd Elmonim',
+                  style:TextStyle(
+                      fontSize: 20
+                  ) ,)),
+
+            TextButton(
+                onPressed: ()async
+            {
+              const url = "https://flutter.io";
+              if (await canLaunch(url) != null)
+                await launch(url);
+              else
+                // can't launch url, there is some error
+                throw "Could not launch $url";
+
+            },
+                child: Text('Ahmed Mohamed Abdo',
+            style:TextStyle(
+              fontSize: 20
+            ) ,)),
+            TextButton(onPressed: ()async
+            {
+              const url = "https://flutter.io";
+              if (await canLaunch(url) != null)
+                await launch(url);
+              else
+                // can't launch url, there is some error
+                throw "Could not launch $url";
+
+            },
+                child: Text('Ahmed Mohamed Shiref',
+            style:TextStyle(
+              fontSize: 20
+            ) ,)),
+            TextButton(
+                onPressed: ()async
+            {
+              const url = "https://flutter.io";
+              if (await canLaunch(url) != null)
+                await launch(url);
+              else
+                // can't launch url, there is some error
+                throw "Could not launch $url";
+
+            },
+                child: Text('Abdelrazek Ali Abdelrazek',
+                  style:TextStyle(
+                      fontSize: 20
+                  ) ,)),
+            TextButton(onPressed: ()async
+            {
+              const url = "https://flutter.io";
+              if (await canLaunch(url) != null)
+                await launch(url);
+              else
+                // can't launch url, there is some error
+                throw "Could not launch $url";
+
+            },
+                child: Text('Nada Tamer Shouman',
+            style:TextStyle(
+              fontSize: 20
+            ) ,)),
+            TextButton(onPressed: ()async
+            {
+              const url = "https://flutter.io";
+              if (await canLaunch(url) != null)
+                await launch(url);
+              else
+                // can't launch url, there is some error
+                throw "Could not launch $url";
+
+            },
+                child: Text('Mai Mahmoud Morshed',
+            style:TextStyle(
+              fontSize: 20
+            ) ,)),
+
+          ],
+        ),
+      ),
+
+    );}
+
+}
+
+
+
+
+
+
+
+
+/*()async
           {
             const url = "https://flutter.io";
             if (await canLaunch(url) != null)
@@ -16,17 +129,4 @@ class AboutUsScreen extends StatelessWidget {
             else
             // can't launch url, there is some error
             throw "Could not launch $url";
-          },
-          child: Text('Ahmed'),
-        ),
-      ),
-    );}
- Future _launchURL() async {
-    const url = 'https://google.com';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-}
+          },*/
