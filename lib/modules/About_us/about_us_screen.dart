5279cd38630c96334f380d56239981f3dbceba4b
Ahmed Shiref <103084673+Ahmedshiref72@users.noreply.github.com>
2022-07-16 13:55:45 +0200

    pass model

--- lib/modules/About_us/about_us_screen.dart
+++ lib/modules/About_us/about_us_screen.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text('About Us') ,
-
+      appBar: AppBar(title: Text('About Us '),),
+      body: Center(
+        child: TextButton(
+          onPressed: ()async
+          {
+            const url = "https://flutter.io";
+            if (await canLaunch(url) != null)
+            await launch(url);
+            else
+            // can't launch url, there is some error
+            throw "Could not launch $url";
+          },
+          child: Text('Ahmed'),
+        ),
       ),
-      body: Text('Contact With Us',style: TextStyle(fontSize: 30),),
-    );
+    );}
+ Future _launchURL() async {
+    const url = 'https://google.com';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
